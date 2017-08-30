4eafa7ed570fe302f70ca8ff5bee7f03e452b3a7
Nithya <Nithya.Vijayasekaran@inmarsat.com>
2017-08-30 16:12:11 +0100

    coisdd

--- pipeline.bat
+++ pipeline.bat
@@ -1,4 +1,4 @@
 node {
    stage 'Run JMeter Test'
-   bat 'D:\apache-jmeter-3.1\apache-jmeter-3.1\bin\jmeter.bat -n -t D:\Jmeter_scripts\EB\EB.jmx -l test.jtl'
+   bat 'D:/apache-jmeter-3.1/apache-jmeter-3.1/bin/jmeter.bat -n -t D:/Jmeter_scripts/EB/EB.jmx -l test.jtl'
 }
